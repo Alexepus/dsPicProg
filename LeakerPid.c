6d2aaa1a881a02b552ef6669c3e1316a3b90c2c7
Alexepus <alexepus@gmail.com>
2018-06-27 07:05:09 +0700

    Расчет среднего для АЦП вакууметров

--- LeakerPid.c
+++ LeakerPid.c
@@ -1,4 +1,7 @@
+#include "Main.h"
+
 void task_LeakerPid_body(void)
 {
-    
+	ADCDataAveraged[4]=(ADCDataTemp[0][4]+ADCDataTemp[1][4]+ADCDataTemp[2][4]+ADCDataTemp[3][4])>>2;
+	ADCDataAveraged[5]=(ADCDataTemp[0][5]+ADCDataTemp[1][5]+ADCDataTemp[2][5]+ADCDataTemp[3][5])>>2;    
 }
